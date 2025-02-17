f74cd79f8159d08a4ffea75c05f264add093160c
Denis Hendriansah <denishendriansah@Deniss-MacBook-Air.local>
2025-02-17 21:20:46 +0700

    TestSuite1

--- cypress/e2e/TestSuite1.cy.js
+++ cypress/e2e/TestSuite1.cy.js
@@ -5,20 +5,49 @@
   You may structure or name your file however you see fit as this is just a template
 */
 
-describe("Test Suite 1", () => {
+describe('99.co Singapore Home Page', () => {
   beforeEach(() => {
-    //Optional
-  });
+    cy.visit('https://www.99.co/singapore')
+  })
 
-  it("Test Case 1", () => {
-    //Write your automation script here for Test Case 1
-  });
+  it('should contain the H2 title "Featured Projects"', () => {
+    cy.get('div._1__lJ').contains('Featured projects').should('be.visible')
+  })
 
-  it("Test Case 2", () => {
-    //Optional
-  });
+  it('should contain the H2 title "Popular Projects"', () => {
+    cy.get('div._1__lJ').contains('Popular projects').should('be.visible')
+  })
 
-  it("Test Case 3", () => {
-    //Optional
-  });
-});
+  it('should contain the H2 title "Find your property value instantly"', () => {
+    cy.get('h2').contains('Find your property value instantly').should('be.visible')
+  })
+
+  it('should contain the H2 title "Listings with videos"', () => {
+    cy.get('div._1__lJ').contains('Listings with videos').should('be.visible')
+  })
+  
+  // Can't find this title in the page
+  /* it('should contain the H2 title "View these via video call"', () => {
+    cy.contains('View these via video call').should('be.visible')
+  }) */
+
+  it('should contain the H2 title "Popular listings"', () => {
+    cy.get('div._1__lJ').contains('Popular listings').should('be.visible')
+  })
+
+  it('should contain the H2 title "Featured stories"', () => {
+    cy.get('div._1__lJ').contains('Featured stories').should('be.visible')
+  })
+
+  it('should contain the H2 title "Listings found only on 99"', () => {
+    cy.get('div._1__lJ').contains('Listings found only on 99').should('be.visible')
+  })
+
+  it('should contain the H2 title "Latest New Launches"', () => {
+    cy.get('h2').contains('Latest New Launches').should('be.visible')
+  })
+
+  it('should contain the H2 title "Explore 99.co"', () => {
+    cy.contains('Explore 99.co').should('be.visible')
+  })
+})
